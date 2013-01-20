ed7dbe8eb12505ae3bf2cd50d19f1bcca45d8780
Jamie Roth <roth.j@me.com>
2013-01-19 21:38:35 -0500

    Add webfonts, update layout, add javasctipt.js

---
+++ javascript.js
@@ -0,0 +1,19 @@
+// $(document).ready(function() {
+//    alert('hi');
+// });
+
+$(document).ready() {
+	$('target').scroll(function(){
+		$('.navbar > img').hide("slow");
+}
+})
+
+// $(document).ready(function(event) {
+// 		event.preventDefault();
+// 		$('form#contact_form').submit(
+// 			function() {
+// 			$(form#contact_form').hide();
+// 			$('div#post_submit').show();
+// )};
+// 	)};
+
